cde4b8c63701d4f77fbe2dcf681b200fe2cd5e9c
KangYunJi <dbswl701@ajou.ac.kr>
2021-05-30 17:09:18 +0900

    remove search

--- src/main/java/health_functional_food/DomThread.java
+++ src/main/java/health_functional_food/DomThread.java
@@ -32,16 +32,12 @@ public class DomThread extends Thread{
 		ResultSet r = null;
 		Statement stm =null;
 		Connection conn = null;
-		
-		// 변수 자료형 여러가지로 손봐야 할 필요성 있음!! varchar로 할건지, 아니면 integer 형으로 할 것인지
-		// -> 정렬같은거 할 필요 없으면 그냥 varchar로 두면 될텐데 그럼 보고서에 적는게 너무 단조로워짐.
-
+	
 		try {
 
 			Scanner scan = new Scanner(System.in);
 
 			System.out.println("Connecting PostgreSQL database");
-			// JDBC를 이용해 PostgreSQL 서버 및 데이터베이스 연결
 			
 		
 			String connectionUrl = "jdbc:postgresql:health_functional_food";
@@ -54,11 +50,9 @@ public class DomThread extends Thread{
 	
 
 			System.out.println("< < Creating Acknowledgment, Item, Retail, Gmp relations > >");
-			// 4개 테잉블 생성 : Create table문 이용
 
-			// commit!
 			stm = conn.createStatement();
-			// table 생성문 보고서에 수정 필요!
+
 			String create = "create table Acknowledgment(BSSH_NM varchar(30), PRDCT_NM varchar(200), HF_FNCLTY_MTRAL_RCOGN_NO varchar(20), IFTKN_ATNT_MATR_CN varchar(100), ADDR varchar(100), PRIMARY_FNCLTY varchar(300), DAY_INTK_HIGHLIMIT varchar(60));\r\n"
 					+ "create table Item(INTK_UNIT varchar(10), IFTKN_ATNT_MATR_CN varchar(200), DAY_INTK_HIGHLIMIT varchar(30), PRDCT_NM varchar(70), DAY_INTK_LOWLIMIT varchar(30), PRIMARY_FNCLTY varchar(310));\r\n"
 					+ "create table Retail(PRMS_DT integer, BSSH_NM varchar(30), TELNO varchar(12), LCNS_NO bigint, PRSDNT_NM varchar(20), INSTT_NM varchar(20), LOCP_ADDR varchar(100));\r\n"
@@ -70,7 +64,6 @@ public class DomThread extends Thread{
 			
 			System.out.println("< < Insert 시작 > >");
 
-			//// 여기서 parsing 부르고 거기 안에서 또  4개 table로 나눠서 부른다.
 
 			Parsing par = new Parsing();
 			System.out.println("Acknowledgment 시작");
@@ -93,58 +86,11 @@ public class DomThread extends Thread{
 
 			
 			
-			
-			//// 위에까지 xml파일을 db로 불러오는 것 까지 성공
-			//// 이제 db에 있는거 search 하는것만 남음
-
-			// 넣고자 하는것을 input이라고 두고
 			conn.close();
-			Search search = new Search();
-			search.init();
-			input = "추출물";
-			
-			System.out.println(search.food(input));
-			//search.regist(input, p, conn, r);
-			//search.food(input, p, conn, r);
-			
-			
-			
-			//지윤이가 set으로 키워드 넘겨주고
-			//내가 키워드로 ㅣ나ㅓㅇㄹ니어린 해서 결과 저장하면 get으로 들고갈 수 있게끔
-			
-
 			
 		} catch(SQLException ex) {
 			throw ex;
 		}
 	}
-	
 
-	public String getItem() throws SQLException{
-		Search search = new Search();
-		search.init();
-		return search.food(this.input);
-	}
-	
-	public String getCompany() throws SQLException{
-		Search search = new Search();
-		search.init();
-		return search.regist(input);
-	}
-	
-	public String getGmp() throws SQLException{
-		Search search = new Search();
-		search.init();
-		return search.factory_gmp(input);
-	}
-	
-	public void setItemName(String itemName) {
-		this.input = itemName;
-		System.out.println("setItemName" + this.input);
-	}
-	
-	public void setCompanyName(String companyName) {
-		this.input = companyName;
-	}
-	
 }
--- src/main/java/health_functional_food/Parsing.java
+++ src/main/java/health_functional_food/Parsing.java
@@ -22,7 +22,6 @@ public class Parsing {
 			String sql = "insert into Acknowledgment values(?,?,?,?,?,?,?)";
 
 			for(int i = 0; i<list.getLength(); i++) {
-				System.out.println(i);
 				Node node = list.item(i);
 				if(node.getNodeType() == Node.ELEMENT_NODE) {
 					Element eElement = (Element)node;
@@ -36,7 +35,6 @@ public class Parsing {
 					stmt.setString(7, getTagValue("DAY_INTK_CN", eElement));
 					
 					stmt.executeUpdate();
-					//System.out.println("sucess to save");
 				}
 			}
 		} catch (SQLException e) {
@@ -52,7 +50,6 @@ public class Parsing {
 			String sql = "insert into Item values(?,?,?,?,?,?)";
 
 			for(int i = 0; i<list.getLength(); i++) {
-				System.out.println(i);
 				Node node = list.item(i);
 				if(node.getNodeType() == Node.ELEMENT_NODE) {
 					Element eElement = (Element)node;
@@ -65,7 +62,6 @@ public class Parsing {
 					stmt.setString(6, getTagValue("PRIMARY_FNCLTY", eElement));
 					
 					stmt.executeUpdate();
-					//System.out.println("sucess to save");
 				}
 			}
 		} catch (SQLException e) {
@@ -81,7 +77,6 @@ public class Parsing {
 			String sql = "insert into Gmp values(?,?,?,?,?)";
 
 			for(int i = 0; i<list.getLength(); i++) {
-				System.out.println(i);
 				Node node = list.item(i);
 				if(node.getNodeType() == Node.ELEMENT_NODE) {
 					Element eElement = (Element)node;
@@ -93,7 +88,6 @@ public class Parsing {
 					stmt.setString(5, getTagValue("APPN_CANCL_DT", eElement));
 					
 					stmt.executeUpdate();
-					//System.out.println("sucess to save");
 				}
 			}
 		} catch (SQLException e) {
@@ -104,14 +98,12 @@ public class Parsing {
 	}
 	
 	
-	// integer가 아니고 long..... 이것도 수정 필요!
 	public void Retail(NodeList list, Connection conn) {
 		
 		try {
 			String sql = "insert into Retail values(?,?,?,?,?,?,?)";
 
 			for(int i = 0; i<list.getLength(); i++) {
-				System.out.println(i);
 				Node node = list.item(i);
 				if(node.getNodeType() == Node.ELEMENT_NODE) {
 					Element eElement = (Element)node;
@@ -124,7 +116,6 @@ public class Parsing {
 					stmt.setString(6, getTagValue("INSTT_NM", eElement));
 					stmt.setString(7, getTagValue("LOCP_ADDR", eElement));
 					stmt.executeUpdate();
-					//System.out.println("sucess to save");
 				}
 			}
 		} catch (SQLException e) {
@@ -140,7 +131,6 @@ public class Parsing {
 	
 	
 	public static String getTagValue(String tag, Element eElement) {
-		//System.out.println("getTagValue!!!");
 		NodeList nlist = eElement.getElementsByTagName(tag).item(0).getChildNodes();
 		Node nValue = (Node)nlist.item(0);
 		if(nValue==null) return null;
@@ -159,17 +149,12 @@ public class Parsing {
 			serviceName = "I0630";
 		
 		
-		// 여기서 각각 url 들고오고, 각 변수에 맞게 parsing 한 다음 insert문으로 간다?
 		try {
-			//System.out.println("run()!!!");
 		
 			String url = "http://openapi.foodsafetykorea.go.kr/api/384fb31b44854692a1aa/"+serviceName+"/xml/1/1000";
 		    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 		    DocumentBuilder db = dbf.newDocumentBuilder();
 		    
-			// 파싱
-		    //System.out.println("파싱!!!!!!");
-		  
 		    Document doc = db.parse(url);
 		    doc.getDocumentElement().normalize();
 
@@ -177,7 +162,6 @@ public class Parsing {
 		    System.out.println(list.getLength());
 		    
 		    
-		    // 그냥 입력부터 serviceName형식으로 주면 코드는 줄어들텐데...
 		    if (relation == "Acknowledgment") Acknowledgment(list, conn);
 		    else if (relation == "Item") Item(list, conn);
 		    else if (relation == "Retail") Retail(list, conn);
